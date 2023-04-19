7ad15b8a26d463576caec4f7e7277060153d7fe7
Kaitao Chen <79537350+kxc663@users.noreply.github.com>
2023-04-18 21:27:08 -0400

    Update: add saerch function, calculate price increase, remote storage

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const PORT = 8000
 const express = require('express');
-
-const app = express();
+const fs = require("fs");
 const cors = require('cors');
 const path = require('path');
 const cheerio = require('cheerio');
 
+const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 const static_path = path.join(__dirname, "public");
@@ -16,6 +16,7 @@ const https = require('https');
 const baseUrl = 'https://steamcommunity.com/market/search';
 const appid = 730;
 
+const products = {};
 // get the data from the market using url above
 function getMarketData(url) {
     return new Promise((resolve, reject) => {
@@ -49,22 +50,28 @@ const getAllPagesHtml = async () => {
             });
         });
         console.log("page" + i + "success");
-        await new Promise(r => setTimeout(r, 2000)); // Wait 2 seconds before fetching next page
+        await new Promise(r => setTimeout(r, 5000)); // Wait 5 seconds before fetching next page
     }
     console.log("productURL success!");
-    const products = {};
-    for (let i = 0; i < productUrls.length; i++) {
+
+    for (let i = 0; i < productUrls.length - 5; i++) {
         const html = await getMarketData(productUrls[i]);
         const $ = cheerio.load(html);
         const name = $('.market_listing_item_name').text().trim();
+        const icon_src = $('.market_listing_item_img').attr('srcset');
         productHtml.push(html);
-        console.log("product" + i + "success")
+        console.log("product" + i + " success")
         const priceHistoryData = getPriceHistoryData(html);
+        priceHistoryData.push({ icon: icon_src });
         products[name] = priceHistoryData;
         console.log(products);
-        console.log(getPriceIncreasePercentage(priceHistoryData, 7));
-        await new Promise(r => setTimeout(r, 2000)); // Wait 2 seconds before fetching next page
+        await new Promise(r => setTimeout(r, 5000)); // Wait 5 seconds before fetching next page
     }
+    fs.writeFile("data.json", JSON.stringify(products), "utf8", function (err) {
+        if (err) {
+            console.log(err);
+        }
+    });
     return products;
 };
 
@@ -85,19 +92,44 @@ const getPriceHistoryData = (html) => {
     return priceHistoryData;
 };
 
-function getCurrentPrice(priceHistoryData) {
-    return priceHistoryData[priceHistoryData.length - 1].price;
-}
-
-function getPriceIncreasePercentage(data, xDays) {
-    if (data.length <= xDays) {
-        return null;
+function getPriceIncrementPercentage(data, days) {
+    const dataLength = data.length;
+    if (dataLength < 2) {
+        throw new Error("Not enough data points to calculate percentage.");
     }
-    const latestPrice = getCurrentPrice(data);
-    const priceXDaysAgo = data[data.length - 1 - xDays].price;
-    const percentageIncrease = ((latestPrice - priceXDaysAgo) / priceXDaysAgo) * 100;
-    return percentageIncrease;
+    const mostRecentData = data[dataLength - 1];
+    const daysAgoIndex = dataLength - 1 - (days * 24); // Assuming there is one data point per hour
+    if (daysAgoIndex < 0) {
+        throw new Error("Not enough data points to go back the specified number of days.");
+    }
+    const daysAgoData = data[daysAgoIndex];
+    const startingPrice = daysAgoData.price;
+    const endingPrice = mostRecentData.price;
+    const priceIncrementPercentage = ((endingPrice - startingPrice) / startingPrice) * 100;
+    return priceIncrementPercentage;
 }
 
-getAllPagesHtml();
+app.get('/update', async (req, res) => {
+    // if data.json is not empty, then send the data.json
+    // if data.json is empty or not exist, then update
+    if (fs.existsSync("data.json") == false || fs.readFileSync("data.json", "utf8") == "") {
+        const data = await getAllPagesHtml();
+        res.send(data);
+    } else {
+        res.send(fs.readFileSync("data.json", "utf8"));
+    }
+    console.log("Update Success");
+});
+
+app.get('/search', (req, res) => {
+    const name = req.query.q;
+    console.log(name);
+    const data = JSON.parse(fs.readFileSync("data.json", "utf8"));
+    if (name in data) {
+        res.send(data[name]);
+    } else {
+        res.send("Not Found");
+    }
+});
+
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
--- public/index.js
+++ public/index.js
@@ -1,5 +1,122 @@
-$("#collect").click(function() {
-    $.get("/collect-data", function(data) {
-        console.log(data);
+let receivedData = {};
+
+function init(){
+    $("#displayTable").hide();
+    if($("#select").is(":checked")){
+        $("#searchInput").prop("disabled", true);
+    }
+}
+
+$("#update").click(function () {
+    $.get("/update", function (data) {
+        //console.log(data);
+        receivedData = data;
+        generateTable(data);
     });
 });
+
+$("#select").change(function () {
+    const isChecked = $(this).is(":checked");
+    $("#searchInput").prop("disabled", isChecked);
+});
+
+$("#current").change(function () {
+    const isChecked = $(this).is(":checked");
+    $("#days").prop("disabled", isChecked);
+});
+
+$("#searchButton").click(async function () {
+    const searchName = $("#searchInput").val().trim();
+    if (searchName === "" && !$("#select").is(":checked")) {
+        alert("Please enter a search query.");
+        return;
+    } else if ($("#select").is(":checked")) {
+        $("#displayTable td").remove();
+        const result = await searchAll();
+        console.log(result);
+        generateTable(result);
+    } else {
+        try {
+            const result = await searchWeapon(searchName);
+            if (result != null) {
+                $("#displayTable td").remove();
+                generateTable(result);
+                console.log(result);
+            }
+        } catch (error) {
+            console.error("Error searching for weapon:", error);
+        }
+    }
+});
+
+async function searchWeapon(query) {
+    const response = await $.get("/search", { q: query });
+    if (response === "Not Found") {
+        alert("No weapon found with that name.");
+        return;
+    } else if (response.error) {
+        throw new Error(response.error);
+    } else {
+        var product = {};
+        product[query] = response;
+        return product;
+    }
+}
+
+async function searchAll(){
+    const response = $.get("/update", function (data) {});
+    return response;
+}
+
+function generateTable(data) {
+    $("#displayTable").show();
+    const tableBody = document.getElementById("tableBody");
+    for (const name in data) {
+        const weaponData = data[name];
+        const currentPrice = weaponData[weaponData.length - 2].price;
+        const iconUrl = weaponData[weaponData.length - 1].icon;
+        const row = document.createElement("tr");
+        const nameCell = document.createElement("td");
+        nameCell.textContent = name;
+        row.appendChild(nameCell);
+        const iconCell = document.createElement("td");
+        const iconImg = document.createElement("img");
+        iconImg.src = iconUrl;
+        iconImg.width = 80;
+        iconImg.height = 80;
+        iconCell.appendChild(iconImg);
+        row.appendChild(iconCell);
+        const priceCell = document.createElement("td");
+        priceCell.textContent = `$${currentPrice.toFixed(2)}`;
+        row.appendChild(priceCell);
+        const days = $("#days").val();
+        const priceIncrementPercentage = getPriceIncrementPercentage(weaponData, days);
+        const priceIncreaseCell = document.createElement("td");
+        if(priceIncrementPercentage != "less" && priceIncrementPercentage != "Not enough data") {
+            priceIncreaseCell.textContent = `${priceIncrementPercentage.toFixed(2)}%`;
+        } else {
+            priceIncreaseCell.textContent = "--";
+        }
+        row.appendChild(priceIncreaseCell);
+        tableBody.appendChild(row);
+    }
+}
+
+function getPriceIncrementPercentage(data, days) {
+    const dataLength = data.length;
+    if (dataLength < 2) {
+        return "less";
+    }
+    const mostRecentData = data[dataLength - 2];
+    const daysAgoIndex = dataLength - 1 - (days * 24); // Assuming there is one data point per hour
+    if (daysAgoIndex < 0) {
+        return "Not enough data";
+    }
+    const daysAgoData = data[daysAgoIndex];
+    const startingPrice = daysAgoData.price;
+    const endingPrice = mostRecentData.price;
+    const priceIncrementPercentage = ((endingPrice - startingPrice) / startingPrice) * 100;
+    return priceIncrementPercentage;
+}
+
+init();
